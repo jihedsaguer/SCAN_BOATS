bafd73139d82c01c7c392b3943da84cc14edce18
jihedsaguer <92057955+jihedsaguer@users.noreply.github.com>
2024-07-30 15:07:09 +0200

    final

--- js/app.js
+++ js/app.js
@@ -2,34 +2,132 @@ document.addEventListener('DOMContentLoaded', () => {
     const chartConfigs = [
         {
             id: 'line-chart-1',
-            label: "Working On",
             data: [/* your data here */],
             labels: ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'],
             images: [
-                { src: '/images/hero_1.jpg', x: 5, y: 80, details: 'Boat 1 details', popupId: 'popup-chart-1' },
-                { src: '/images/boat1.jpg', x: 1, y: 200, details: 'Boat 2 details', popupId: 'popup-chart-1' },
-                ]
+                {
+                    src: '/images/C805-removebg-preview.png',
+                    x: 2,
+                    y: 180,
+                    details: 'Boat A: This is a detailed description of Boat A. It includes its features, usage, and other relevant information.',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/3dmodel2.png', '/images/3dmodel2.png', '/images/3dmodel2.png']
+                },
+                {
+                    src: '/images/C835-removebg-preview.png',
+                    x: 3,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C805-removebg-preview.png', '/images/C881-gallery2.jpg', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C840-removebg-preview.png',
+                    x: 4,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C805-removebg-preview.png', '/images/C881-gallery2.jpg', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C855.png',
+                    x: 5,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C805-removebg-preview.png', '/images/C881-gallery2.jpg', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C859-removebg-preview.png',
+                    x: 10,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C863-removebg-preview.png',
+                    x: 3.8,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/CC869.png',
+                    x: 9,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C870-removebg-preview.png',
+                    x: 8,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C871-removebg-preview.png',
+                    x: 7.5,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C871-removebg-preview.png',
+                    x: 7.5,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C881-removebg-preview.png',
+                    x: 7.5,
+                    y: 100,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+                {
+                    src: '/images/C8900-removebg-preview.png',
+                    x: 9.5,
+                    y: 100,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-1',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C872-removebg-preview.png', '/images/C881-gallery3.jpg']
+                },
+
+                // Add more images if needed
+            ]
         },
         {
             id: 'line-chart-2',
-            label: "Already Contributed",
             data: [/* your data here */],
             labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
             images: [
-                { src: '/images/hero_2.jpg', x: 8, y: 90, details: 'Contribution 1 details', popupId: 'popup-chart-2' },
-                { src: '/images/contrib_2.jpg', x: 2, y: 180, details: 'Contribution 2 details', popupId: 'popup-chart-2' },
-
+                {
+                    src: '/images/C872-removebg-preview.png',
+                    x: 2,
+                    y: 180,
+                    details: 'Contribution 2 details',
+                    popupId: 'popup-chart-2',
+                    gallery: ['/images/C872-removebg-preview.png', '/images/C881-gallery2.jpg', '/images/C881-gallery3.jpg']
+                },
             ]
         },
         {
             id: 'line-chart-3',
-            label: "Future Plan",
             data: [/* your data here */],
             labels: ['2022', '2023', '2024', '2025', '2026', '2027', '2028'],
             images: [
-                { src: '/images/person_1.jpg', x: 1, y: 120, details: 'Future Plan 1 details', popupId: 'popup-chart-3' },
-                { src: '/images/hero_2.jpg', x: 3, y: 210, details: 'Future Plan 2 details', popupId: 'popup-chart-3' },
-
+                { src: '/images/C840-removebg-preview.png', x: 4, y: 120, details: 'Boat: C840', popupId: 'popup-chart-3' },
+                { src: '/images/C8900-removebg-preview.png', x: 1, y: 120, details: 'Future Plan 1 details', popupId: 'popup-chart-3' },
+                { src: '/images/C881-removebg-preview.png', x: 3, y: 210, details: 'Future Plan 2 details', popupId: 'popup-chart-3' },
             ]
         }
     ];
@@ -67,7 +165,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             const { offsetX, offsetY } = event;
                             if (offsetX >= xPos - imgWidth / 2 && offsetX <= xPos + imgWidth / 2 &&
                                 offsetY >= yPos - imgHeight / 2 && offsetY <= yPos + imgHeight / 2) {
-                                showPopup(img.details, xPos, yPos, img.popupId);
+                                showPopup(img.details, xPos, yPos, img.popupId, img.src, img.gallery);
                             }
                         });
                     };
@@ -76,14 +174,21 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-    function showPopup(details, x, y, popupId) {
+    function showPopup(details, x, y, popupId, imageSrc, gallery) {
         const popup = document.getElementById(popupId);
         if (popup) {
+            const galleryImages = gallery ? gallery.map(src =>
+                `<img src="${src}" alt="Gallery Image" style="width: 100px; height: auto; margin: 5px;">`
+            ).join('') : '';
+
             popup.innerHTML = `
-              <div class="popup-content">
+            <div class="popup-content">
                 <strong>${details}</strong>
                 <br>
-                <button class="more-details-btn" onclick="window.location.href='./details.html?id=${popupId}'">More Details</button>
+                <div class="gallery" style="display: none;">
+                    ${galleryImages}
+                </div>
+                <button class="more-details-btn" onclick="window.location.href='./details.html?id=${encodeURIComponent(popupId)}&image=${encodeURIComponent(imageSrc)}&gallery=${encodeURIComponent(gallery.join(','))}&details=${encodeURIComponent(details)}'">More Details</button>
             </div>
         `;
 
@@ -96,17 +201,21 @@ document.addEventListener('DOMContentLoaded', () => {
                 popup.classList.remove('show');
                 popup.classList.add('hide');
             }, 5000); // Hide after 5 seconds
+        } else {
+            console.error(`Popup with ID ${popupId} not found.`);
         }
     }
 
+
     chartConfigs.forEach(config => {
-        new Chart(document.getElementById(config.id).getContext('2d'), {
+        const ctx = document.getElementById(config.id).getContext('2d');
+
+        new Chart(ctx, {
             type: 'line',
             data: {
                 labels: config.labels,
                 datasets: [{
                     data: config.data,
-                    label: config.label,
                     borderColor: "#ffffff",
                     fill: false,
                     borderWidth: 2
